1c736b158da261143cecfbd7e0cff81dc21b432a
amine <amineorb03@gmail.com>
2025-04-22 01:30:06 +0100

    connect database

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,8 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2025@localhost/student_performance_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Amine2025@@db.avgboakdzhkltpwkuezx.supabase.co:5432/postgres'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 db = SQLAlchemy(app)
